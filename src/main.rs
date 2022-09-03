7900d7fcd102f322b9de93359fbe943cf0ccecc4
charlescostello <ccostello32@gatech.edu>
2022-09-03 16:37:48 -0600

    particle and position components

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Particle;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32
+}
+
+fn create_particles(mut commands: Commands) {
+    commands.spawn().insert(Particle).insert(Position {x: 1.0, y: 1.0});
+    commands.spawn().insert(Particle).insert(Position {x: 2.0, y: 2.0});
+}
+
+fn print_position(query: Query<&Position, With<Particle>>) {
+    for position in query.iter() {
+        println!("This particle is at position: ({}, {})", position.x, position.y)
+    }
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(create_particles)
+        .add_system(print_position)
+        .run();
 }
